58fe07a324d1a066c29ad7be6cfc73012971a761
cpymchn <edu.markb@gmail.com>
2014-01-29 21:00:13 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RollYourOwnRegistry::Application.config.secret_key_base = '8dc45b3b07aa64c665c3a242e204445079f61cae0f8c07eea374d8c5ecfdf6f0c8633b8515d1b5ff194d9aa514c02a21a22beee16be65ca3223eda032bb31a74'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RollYourOwnRegistry::Application.config.session_store :cookie_store, key: '_roll-your-own-registry_session'
